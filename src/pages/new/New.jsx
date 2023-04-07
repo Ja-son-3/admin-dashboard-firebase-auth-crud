131205714ddeccb22df0113a75fe65bd54dd93ed
Jason Huang <jason97521@gmail.com>
2023-04-07 08:21:56 -0400

    create user in firebase db

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -3,10 +3,36 @@ import Sidebar from "../../components/sidebar/Sidebar"
 import Navbar from "../../components/navbar/Navbar"
 import { DriveFolderUploadOutlined } from "@mui/icons-material"
 import { useState } from "react"
+import { addDoc, collection, doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { auth, db } from "../../firebase"
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const New = ({inputs, title}) => {
-
   const [file,setFile] = useState("")
+  const [data,setData] = useState({})
+
+  const handleInput = (e) => {
+    const id = e.target.id;
+    const value = e.target.value;
+
+    setData({...data, [id]: value })
+  }
+
+  console.log(data)
+
+  const handleAdd = async (e) =>{
+    e.preventDefault()
+    try {
+      const res = await createUserWithEmailAndPassword(auth,data.email,data.password)
+      await setDoc(doc(db, "users", res.user.uid), {
+        ...data,
+        timeStamp: serverTimestamp()
+      });
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div className="new">
         <Sidebar />
@@ -20,7 +46,7 @@ const New = ({inputs, title}) => {
               <img src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"} alt="" />
             </div>
             <div className="right">
-              <form>
+              <form onSubmit={handleAdd}>
                 <div className="formInput">
                   Image: <label htmlFor="file"><DriveFolderUploadOutlined className="icon"/></label>
                   <input type="file" id="file" onChange={(e)=>setFile(e.target.files[0])} style={{display: "none",}}/>
@@ -29,10 +55,10 @@ const New = ({inputs, title}) => {
                 {inputs.map(input=>(
                   <div className="formInput" key={input.id}>
                     <label>{input.label}</label>
-                    <input type={input.type} placeholder={input.placeholder} />
+                    <input id={input.id} type={input.type} placeholder={input.placeholder} onChange={handleInput}/>
                   </div>
                 ))}
-                <button>Send</button>
+                <button type="submit">Send</button>
               </form>
             </div>
           </div>
